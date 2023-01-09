a387d72bc775690177b9f303ea59d664cdb0f6b6
Ethan Heinzen <heinzen.ethan@mayo.edu>
2023-01-09 08:30:30 -0600

    reorganize

--- R/tv.R
+++ R/tv.R
@@ -1,24 +1,3 @@
-#' Example data for time-varying
-#'#' Example data for time-varying
-#'
-#' @format A list
-#' \describe{
-#'   \item{data}{The data}
-#'   \item{specs}{The specs}
-#' }
-#' @seealso \code{\link{tv}}
-"tv_example"
-
-
-#' @importFrom magrittr %>%
-#' @export
-magrittr::`%>%`
-
-
-#' @importFrom rlang .data
-NULL
-
-
 #' Create a time-varying dataset
 #'
 #' @param x A data.frame with four columns: <id>, "feature", "datetime", "value"
@@ -41,56 +20,6 @@ NULL
 #'                time_units = "days", id = "mcn")
 #' @name tv
 NULL
-
-#' @rdname tv
-#' @export
-tv_count <- function(x, ...) {
-  nrow(x)
-}
-
-#' @rdname tv
-#' @export
-tv_any <- function(x, ...) {
-  +(nrow(x) > 0)
-}
-
-
-#' @rdname tv
-#' @export
-tv_lvcf <- function(x, ...) {
-  if(nrow(x) > 0) x$value[which.max(x$datetime)] else NA_real_
-}
-
-
-#' @rdname tv
-#' @export
-tv_ts <- function(x, current_time, ...) {
-  if(nrow(x) > 0) current_time - max(x$datetime) else NA_real_
-}
-
-#' @rdname tv
-#' @export
-tv_min <- function(x, ...) {
-  if(nrow(x) > 0) min(x$value, na.rm = TRUE) else NA_real_
-}
-
-#' @rdname tv
-#' @export
-tv_max <- function(x, ...) {
-  if(nrow(x) > 0) max(x$value, na.rm = TRUE) else NA_real_
-}
-
-#' @rdname tv
-#' @export
-tv_mean <- function(x, ...) {
-  if(nrow(x) > 0) mean(x$value, na.rm = TRUE) else NA_real_
-}
-
-#' @rdname tv
-#' @export
-tv_sum <- function(x, ...) {
-  sum(x$value, na.rm = TRUE)
-}
 
 #' @rdname tv
 #' @export
---
+++ R/tv_example.R
@@ -0,0 +1,9 @@
+#' Example data for time-varying
+#'
+#' @format A list
+#' \describe{
+#'   \item{data}{The data}
+#'   \item{specs}{The specs}
+#' }
+#' @seealso \code{\link{tv}}
+"tv_example"
